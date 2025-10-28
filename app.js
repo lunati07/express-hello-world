2db193d4978ad3eda4b55db23d67a290d6a4c92f
lunati07 <natilucrezia@gmail.com>
2025-10-28 12:19:04 +0100

    Refactor app.js to use ES6 imports and simplify code

--- app.js
+++ app.js
@@ -1,61 +1,30 @@
-const express = require("express");
+import express from "express";
+import fs from "fs";
+import cors from "cors";
+
 const app = express();
-const port = process.env.PORT || 3001;
+app.use(cors());
 
-app.get("/", (req, res) => res.type('html').send(html));
+const FILE = "counter.json";
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// crea file contatore se non esiste
+if (!fs.existsSync(FILE)) {
+  fs.writeFileSync(FILE, JSON.stringify({ current: 0 }));
+}
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+app.get("/get-code", (req, res) => {
+  const data = JSON.parse(fs.readFileSync(FILE, "utf8"));
+  let current = data.current;
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+  if (current >= 100) {
+    return res.json({ success: false, message: "Codici esauriti." });
+  }
+
+  current++;
+  fs.writeFileSync(FILE, JSON.stringify({ current }));
+
+  const code = `NSG25-${current}-T`;
+  res.json({ success: true, code });
+});
+
+app.listen(10000, () => console.log("✅ Server attivo"));
